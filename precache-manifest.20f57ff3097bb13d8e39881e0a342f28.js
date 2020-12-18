116da4bf25b3f4bd49ac17cc0fea05d7ed8f40ce
Yandi2020 <wangyandi0406@gmail.com>
2020-12-18 18:43:03 +0100

    Updates

---
+++ precache-manifest.20f57ff3097bb13d8e39881e0a342f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da71ac0e4eadeea0381c0f6366d5964",
+    "url": "/React-Redux-Practice/index.html"
+  },
+  {
+    "revision": "0ea5edd0dd6d58f381d8",
+    "url": "/React-Redux-Practice/static/css/main.863f997b.chunk.css"
+  },
+  {
+    "revision": "929f79780749f1558715",
+    "url": "/React-Redux-Practice/static/js/2.296a0efd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Practice/static/js/2.296a0efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea5edd0dd6d58f381d8",
+    "url": "/React-Redux-Practice/static/js/main.47fd0311.chunk.js"
+  },
+  {
+    "revision": "a503675c8a0f8897adb0",
+    "url": "/React-Redux-Practice/static/js/runtime-main.9fd0e9ac.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/React-Redux-Practice/static/media/pokeball.f10bf078.png"
+  }
+]);
